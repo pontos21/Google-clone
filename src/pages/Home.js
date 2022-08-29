dc77238c743ba951e36a35e899a4357c58947783
paul serafim <paul.serafim.3021@gmail.com>
2022-08-29 17:36:51 +1000

    fixed home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,11 +11,11 @@ const Home = () => {
     return (
     <div className="home">
         <div className="home__header">
-            <div className="home__header--left">
+            <div className="home__headerLeft">
                 <Link to='/about'>About</Link>
                 <Link to='/store'>Store</Link>
             </div>
-            <div className="home__header--right">
+            <div className="home__headerRight">
                 <Link to='/gmail'>Gmail</Link>
                 <Link to='/images'>Images</Link>
                 <AppsIcon />
@@ -27,7 +27,7 @@ const Home = () => {
                 src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" 
                 alt=""
             />
-            <div className="home__input--container">
+            <div className="home__inputContainer">
                 <Search />
             </div>
         </div>
